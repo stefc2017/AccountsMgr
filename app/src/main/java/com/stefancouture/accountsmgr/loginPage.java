6e08859ca94677827d5d647b78ec1bd33355e5a3
Stefan <stefc2013@gmail.com>
2017-07-26 15:23:54 -0500

    Master: Created initial master branch Created the initial master branch and set up the GitHub repository Time: 5 mins

---
+++ app/src/main/java/com/stefancouture/accountsmgr/loginPage.java
@@ -0,0 +1,13 @@
+package com.stefancouture.accountsmgr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
