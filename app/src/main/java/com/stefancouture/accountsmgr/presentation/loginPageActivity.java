a8a7b32ad92000d7963d335ecfbb553c0d41bb9e
Stefan <stefc2013@gmail.com>
2017-07-26 15:34:00 -0500

    Issue-1: Set up project structure Created the initial folders to separate each part of the application into tiers Time: 5 mins

--- app/src/main/java/com/stefancouture/accountsmgr/presentation/loginPageActivity.java
+++ app/src/main/java/com/stefancouture/accountsmgr/presentation/loginPageActivity.java
@@ -3,7 +3,7 @@ package com.stefancouture.accountsmgr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class loginPage extends AppCompatActivity {
+public class loginPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
