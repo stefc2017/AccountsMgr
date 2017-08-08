a582f0f6ada0f036d4af10513209c2ed84ebcf56
Stefan <stefc2013@gmail.com>
2017-08-08 15:31:39 -0500

    Issue-11: Add Logout button I added a logout button to the toolbar so the user can logout of their account. Time: 30 mins

--- app/src/main/java/com/stefancouture/accountsmgr/presentation/welcomePageActivity.java
+++ app/src/main/java/com/stefancouture/accountsmgr/presentation/welcomePageActivity.java
@@ -1,10 +1,11 @@
 package com.stefancouture.accountsmgr.presentation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.Menu;
+import android.view.MenuItem;
 import com.stefancouture.accountsmgr.R;
-
 
 public class welcomePageActivity extends AppCompatActivity{
 
@@ -15,8 +16,19 @@ public class welcomePageActivity extends AppCompatActivity{
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_welcome, menu);
+        return true;
+    }
+
+    @Override
     public void onBackPressed(){
         //dont let the user use the back button
         //they need to use logout button
+    }
+
+    public void logout(MenuItem item){
+        Intent intent = new Intent(this, loginPageActivity.class);
+        startActivity(intent);
     }
 }
